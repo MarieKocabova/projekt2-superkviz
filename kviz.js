ca0c95192fa9093a04341e3cb7efbd7e338986fb
NTDELLMARUSKA\Marie <maruspondeli@gmail.com>
2021-04-22 17:45:56 +0200

    funguje generování otázek

--- kviz.js
+++ kviz.js
@@ -1,41 +1,66 @@
 let poleOtazek = [
     {
-        poradi: 1, 
+        cisloOtazky: 1, 
+        foto: 'moncicak.jpg',
         otazkaText: 'Co je ikonická hračka z 80. let?', 
         moznosti: ['Kočičák', 'Mončičák', 'Opičák'],
-        spravnaOdpoved: 1 
+        spravnaOdpoved: 1,
     },
     {
-        poradi: 2,
+        cisloOtazky: 2,
+        foto: 'ovoce.jpg',
         otazkaText: 'Jaké je Matějovo nejoblíbenější ovoce?',
         moznosti: ['Kokos', 'Melounek', 'Jahoda', 'Ani jedna z možností'],
         spravnaOdpoved: 2
     },
     {
-        poradi: 3,
+        cisloOtazky: 3,
+        foto: 'pivo.jpg',
         otazkaText: 'Pro úspěšné absolvování kurzu je potřeba...',
         moznosti: ['Umět JavaScript', 'Chodit po kurzu do hospody'],
         spravnaOdpoved: 0
     }
 ]
 
-let spravneOdpovedi = [];  //tady info o správných odpovědích
-let aktualniOtazka;  //tady udržuju index aktuální otázky
+let mozneOdpovedi = document.querySelector('#odpovedi');
+//let spravneOdpovedi = [];  //tady info o správných odpovědích
+let indexAktualniOtazky;  //tady udržuju index aktuální otázky
 
-console.log(poleOtazek[3])
+console.log(poleOtazek[0].moznosti[0])
 
 //fce vygeneruj otázku
-//1. červený text - na které otázce z kolika jsem
-//2. text otázky
-//3. obrázek
-//4. div s možnostmi viz readme foreach moznosti
+
+function getQuestion(indexAktualniOtazky) {
+    //1. červený text - na které otázce z kolika jsem
+    document.querySelector('#poradi').innerHTML = 'Otázka ' + poleOtazek[indexAktualniOtazky].cisloOtazky + '/' + poleOtazek.length;
+    //2. text otázky
+    document.querySelector('#otazka').innerHTML = poleOtazek[indexAktualniOtazky].otazkaText;
+    //3. obrázek
+    document.querySelector('.foto').src = 'obrazky/' + poleOtazek[indexAktualniOtazky].foto;
+    //4. div s možnostmi viz readme foreach moznosti
+    for (let i = 0; i < poleOtazek[indexAktualniOtazky].moznosti.length; i++) {
+        const odpovedi = document.createElement('ul');
+        odpovedi.setAttribute("id", "odpovedi");
+
+        const moznost = document.createElement('li');
+        moznost.setAttribute("data-odpoved", i);
+        moznost.innerHTML = poleOtazek[indexAktualniOtazky].moznosti[i];
+
+        odpovedi.appendChild(moznost);
+        document.querySelector('#moznosti').appendChild(odpovedi);
+
+    }
+
+}
+getQuestion(0);
+
+
 
 
 //init - vygeneruj otázku[0]
 
 
 //onclick na odpověď
-//1. poznej, kam kliknul
 //1.1. zkontroluj správnost - pomůže asi tohle: https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_global_data 
 
 //2. ulož do proměnné odpověď (pole spravneOdpovedi)
@@ -47,5 +72,5 @@ console.log(poleOtazek[3])
 //2. cyklus foreach otázku - asi vygeneruj samostatný div, kde bude:
     //2.1. pořadí + text otázky
     //2.2. tvoje odpověď: zvolená odpověď (poleOtazek[].moznosti[zvolená])
-    //2.3. if zvolená=správná -> vypiš "To je Správně", else -> Správná odpověď: poleOtazek[].spravnaOdpoved
+    //2.3. if zvolená==správná -> vypiš "To je Správně", else -> Správná odpověď: poleOtazek[].spravnaOdpoved
 //3. text 'Správně je spravneOdpovedi.length z poleOtazek.lenght.... Úspěšnost....%.'
